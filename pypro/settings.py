3345292992159bc57eb3e020f5e0c0b74cb3671c
Tânio Scherer <tanioms3@gmail.com>
2019-10-26 07:41:14 -0300

    Adiciona lib python-decouple e separa a conf DEBUG

--- pypro/settings.py
+++ pypro/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+from decouple import config
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -23,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1#+e^nj!8h5i360ib(z^8lfubn9i*%t)nq-tzgnwve!bmp(#+^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
