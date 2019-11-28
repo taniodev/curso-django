aec0db8d29d6a48766271f03e442426e6a695bf5
Tânio Scherer <tanioms3@gmail.com>
2019-11-28 12:25:26 -0300

    Adiciona a exibição do video de acordo com a slug

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    video = {
-        'titulo': 'Video Aperitivo: Motivação',
-        'vimeo_id': 32801864,
+    videos = {
+        'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 32801864},
+        'instalacao-windows': {'titulo': 'Instalação no Windows', 'vimeo_id': 49363935},
     }
+    video = videos[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
