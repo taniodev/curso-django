5b075c00866af0edb7d17712007a82d5ab168173
Tânio Scherer <tanioms3@gmail.com>
2019-11-29 13:05:35 -0300

    Cria a pagina indice com videos da App aperitivos
    * Renomeia o modulo test_aperitivos.py para test_video.py.
    
    close #6

--- pypro/aperitivos/urls.py
+++ pypro/aperitivos/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from pypro.aperitivos.views import video
+from pypro.aperitivos.views import indice, video
 
 app_name = 'aperitivos'
 
 urlpatterns = [
+    path('', indice, name='indice'),
     path('<slug:slug>', video, name='video'),
 ]
--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 
+videos = [
+    {'slug': 'motivacao', 'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 32801864},
+    {'slug': 'instalacao-windows', 'titulo': 'Instalação no Windows', 'vimeo_id': 49363935},
+]
+
+videos_dct = {dct['slug']: dct for dct in videos}
+
+
+def indice(request):
+    return render(request, 'aperitivos/indice.html', context={'videos': videos})
+
 
 def video(request, slug):
-    videos = {
-        'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 32801864},
-        'instalacao-windows': {'titulo': 'Instalação no Windows', 'vimeo_id': 49363935},
-    }
-    video = videos[slug]
+    video = videos_dct[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
