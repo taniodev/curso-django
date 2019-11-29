b09506838f87f1553d5c7b74fca71e266022dd30
Tânio Scherer <tanioms3@gmail.com>
2019-11-29 17:45:39 -0300

    Adiciona os links dos videos na pagina de indice
    close #11

--- pypro/aperitivos/tests/test_indice.py
+++ pypro/aperitivos/tests/test_indice.py
@@ -19,3 +19,12 @@ def test_status_code(resp):
 ])
 def test_titulo_do_video(resp, titulo):
     assert_contains(resp, titulo)
+
+
+@pytest.mark.parametrize('slug', [
+    'motivacao',
+    'instalacao-windows',
+])
+def test_link_do_video(resp, slug):
+    link = reverse('aperitivos:video', args=(slug,))
+    assert_contains(resp, f'href="{link}"')
