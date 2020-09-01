0a52ed763bd33429d8f4dd23c8485123939da95a
Tânio Scherer <tanioms3@gmail.com>
2020-09-01 20:39:23 -0300

    Adiciona o campo slug ao modelo Modulo

--- pypro/modulos/models.py
+++ pypro/modulos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from ordered_model.models import OrderedModel
 
 
@@ -6,6 +7,10 @@ class Modulo(OrderedModel):
     titulo = models.CharField(max_length=64)
     publico = models.TextField()
     descricao = models.TextField()
+    slug = models.SlugField(unique=True)
 
     class Meta(OrderedModel.Meta):
         pass
+
+    def get_absolute_url(self):
+        return reverse('modulos:detalhe', kwargs={'slug': self.slug})
