4815fcf4cb00d0a48a8f09691cf02379eed0c61c
Tânio Scherer <tanioms3@gmail.com>
2020-10-03 16:05:32 -0300

    Implementa o índice de aulas e módulos

--- pypro/modulos/tests/test_modulos_indice.py
+++ pypro/modulos/tests/test_modulos_indice.py
@@ -2,6 +2,7 @@ import pytest
 from django.urls import reverse
 from model_bakery import baker
 
+from pypro.django_assertions import assert_contains
 from pypro.modulos.models import Aula, Modulo
 
 
@@ -26,3 +27,28 @@ def resp(client, modulos, aulas):
 
 def test_indice_disponivel(resp):
     assert resp.status_code == 200
+
+
+def test_titulo(resp, modulos):
+    for modulo in modulos:
+        assert_contains(resp, modulo.titulo)
+
+
+def test_descricao(resp, modulos):
+    for modulo in modulos:
+        assert_contains(resp, modulo.descricao)
+
+
+def test_publico(resp, modulos):
+    for modulo in modulos:
+        assert_contains(resp, modulo.publico)
+
+
+def test_aulas_titulos(resp, aulas):
+    for aula in aulas:
+        assert_contains(resp, aula.titulo)
+
+
+def test_aulas_urls(resp, aulas):
+    for aula in aulas:
+        assert_contains(resp, aula.get_absolute_url())
