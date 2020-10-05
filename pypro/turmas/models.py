d018f1d28518ecc53155febf2d8780923146bb91
Tânio Scherer <tanioms3@gmail.com>
2020-10-05 16:26:57 -0300

    Cria a estrutura básica da app turmas

---
+++ pypro/turmas/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Turma(models.Model):
+    nome = models.CharField(max_length=64)
+    slug = models.SlugField(max_length=64)
+    inicio = models.DateField()
+    fim = models.DateField()
+
+    def __str__(self):
+        return self.nome
