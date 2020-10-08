2ccf20c719e96ec7b0aa9b8a7d1641328cd65651
Tânio Scherer <tanioms3@gmail.com>
2020-10-08 17:01:37 -0300

    Redireciona para a página de módulos após o login

--- pypro/base/tests/test_login.py
+++ pypro/base/tests/test_login.py
@@ -1,5 +1,6 @@
 import pytest
 from django.urls import reverse
+from model_bakery import baker
 
 
 @pytest.fixture
@@ -10,3 +11,24 @@ def resp(client, db):
 
 def test_status_code(resp):
     assert resp.status_code == 200
+
+
+@pytest.fixture
+def usuario(db, django_user_model):
+    senha = "uma senha qualquer"
+    usuario = baker.make(django_user_model)
+    usuario.set_password(senha)
+    usuario.save()
+    usuario.senha_plana = senha
+    return usuario
+
+
+@pytest.fixture
+def resp_post(client, usuario):
+    resp = client.post(reverse('login'), {'username': usuario.email, 'password': usuario.senha_plana})
+    return resp
+
+
+def test_login_redirect(resp_post):
+    assert resp_post.status_code == 302
+    assert resp_post.url == reverse('modulos:indice')
